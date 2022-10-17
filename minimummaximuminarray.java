76b2bab76a14014bec23ac91fbab6db47734a65e
Suhanipachouri <97778571+Suhanipachouri@users.noreply.github.com>
2022-10-17 21:07:35 +0530

    Create minimummaximuminarray.java

---
+++ minimummaximuminarray.java
@@ -0,0 +1,17 @@
+ class pair  
+{  
+    long first, second;  
+    public pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+} */
+
+class Compute 
+{
+    static pair getMinMax(long a[], long n)  
+    {
+        //Write your code here
+    }
+}
