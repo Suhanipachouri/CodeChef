e8d6fe41e5afc751ce15dbef34216a1b73890e88
Suhanipachouri <97778571+Suhanipachouri@users.noreply.github.com>
2022-11-29 23:29:23 +0530

    Create Fever.java

---
+++ Fever.java
@@ -0,0 +1,18 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int t = sc.nextInt();
+	    for(int i=0;i<t;i++){
+	        
+	    int X = sc.nextInt();
+		if(X>98){
+		    System.out.println("Yes");
+		}
+		else{
+		    System.out.println("No");
+		}
+	    }
+	}
+}
