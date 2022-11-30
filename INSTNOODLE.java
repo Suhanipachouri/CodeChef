15741e8a287ed7cb2a22760fd0dc00caac2fdf9c
Suhanipachouri <97778571+Suhanipachouri@users.noreply.github.com>
2022-12-01 00:57:49 +0530

    Create INSTNOODLE.java

---
+++ INSTNOODLE.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		try{
+		    Scanner sc = new Scanner(System.in);
+		        int X = sc.nextInt();
+		        int Y = sc.nextInt();
+		        int total= X*Y;
+		        System.out.println(total);
+		}
+		catch(Exception e){
+		    return;
+		}
+	}
+}
