3b87d1ab49a0c5abf798075cf42adc1fe94302ef
Suhanipachouri <97778571+Suhanipachouri@users.noreply.github.com>
2022-12-01 00:55:11 +0530

    Create FBC.java

---
+++ FBC.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		try{
+		    Scanner sc = new Scanner(System.in);
+		    int t = sc.nextInt();
+		    for(int i =1;i<=100;i++){
+	     	    int K = sc.nextInt();
+	    	    int X = sc.nextInt();
+	            int Space = K-X;
+	            System.out.println(Space);
+	    	    }  
+		}
+		catch(Exception e){
+		    return;
+		}
+	}
+}
